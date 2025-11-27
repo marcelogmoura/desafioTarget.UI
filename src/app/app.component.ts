a88fd50a08fd64fd22a026056655fd176ded12a3
Marcelo Moura <93623867+marcelogmoura@users.noreply.github.com>
2025-11-27 00:58:52 -0300

    UI finalizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router'; // <--- IMPORTANTE
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
